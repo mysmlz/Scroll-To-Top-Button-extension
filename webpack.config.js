61a5f3310173c801a508d302f5aba33e739686ff
poziworld <poziworld@gmail.com>
2018-07-19 00:54:17 -0700

    New: generate browser-specific manifest.json files*
    * Chromium, Firefox, Edge

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require( 'path' );
+const { List, Map } = require( 'immutable' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+
+const modeDevelopment = process.env.NODE_ENV === 'development';
+
+const defaultConfig = Map( {
+  entry: {
+    'manifest': './src/manifest.json',
+  },
+  output: Map( {
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+    path: path.resolve( __dirname, 'dist' ),
+  } ),
+  module: {
+    rules: [
+      {
+        test: /manifest.json$/,
+        exclude: /node_modules/,
+        loader: 'manifest-loader',
+      },
+    ],
+  },
+  plugins: List( [
+    new CopyWebpackPlugin(
+      [
+        {
+          from: './static',
+          to: './',
+        },
+      ]
+    ),
+  ] ),
+  resolveLoader: {
+    modules: [
+      path.resolve( __dirname, 'src', 'loaders' ),
+      'node_modules',
+    ],
+  },
+  node: {
+    fs: 'empty',
+  },
+  devtool: undefined,
+  watch: modeDevelopment,
+} );
+
+const supportedBrowsers = [
+  'chromium',
+  'firefox',
+  'edge',
+];
+
+module.exports = supportedBrowsers.map( browserName => {
+  return defaultConfig
+    // Separate dist folder for each browser
+    .updateIn(
+      [
+        'output',
+        'path',
+      ],
+      () => path.resolve( __dirname, 'dist', browserName ),
+    )
+    .toJS()
+    ;
+} );
