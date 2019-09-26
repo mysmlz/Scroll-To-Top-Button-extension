1c6c8a3e6756d6360537abe547d601e81a2b48cf
PoziWorld <5961162+PoziWorld@users.noreply.github.com>
2019-09-25 22:26:53 -0700

    Improve: refactor “before:browser:launch” callback for clarity
    #test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,12 @@
-const loadExtension = require( 'cypress-browser-extension-plugin/loader' ).load;
+const extensionLoader = require( 'cypress-browser-extension-plugin/loader' );
 
 module.exports = ( on ) => {
-  on( 'before:browser:launch', loadExtension( {
-    alias: 'sttb',
-    // @todo Grab dynamically?
-    source: './dist/chromium',
-    skipHooks: false,
-  } ) );
+  on( 'before:browser:launch', ( browser = {}, args ) => (
+    extensionLoader.load( {
+      alias: 'sttb',
+      // @todo Grab dynamically?
+      source: './dist/chromium',
+      skipHooks: false,
+    } )( browser, args )
+  ) );
 };
