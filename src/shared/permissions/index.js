ca27ae68188dec2829733a472058ee280bfff989
PoziWorld <5961162+PoziWorld@users.noreply.github.com>
2020-05-03 23:25:49 -0700

    Improve: add browser.permissions.contains fallback in hasPermissions

--- src/shared/permissions/index.js
+++ src/shared/permissions/index.js
@@ -1,19 +1,44 @@
+const ORIGINS_KEY = 'origins';
+const ALL_URLS_ORIGIN = '<all_urls>';
+const PERMISSIONS_KEY = 'permissions';
+const TABS_PERMISSION = 'tabs';
 export const EXPERT_BUTTON_MODES_PERMISSIONS = {
-  origins: [
-    '<all_urls>',
+  [ ORIGINS_KEY ]: [
+    ALL_URLS_ORIGIN,
   ],
-  permissions: [
-    'tabs',
+  [ PERMISSIONS_KEY ]: [
+    TABS_PERMISSION,
   ],
 };
 
 /**
  * Expert button modes require certain permissions. Check whether those permissions have been granted.
  *
- * @return {boolean}
+ * @returns {boolean}
  */
 
 export async function hasPermissions() {
-  // @todo Figure out why browser.permissions.contains returns false for origins <all_urls>, whereas browser.permissions.getAll lists it. Related: {@link https://bugs.chromium.org/p/chromium/issues/detail?id=931816}
-  return browser.permissions.contains( EXPERT_BUTTON_MODES_PERMISSIONS );
+  const permissionsGranted = await browser.permissions.contains( EXPERT_BUTTON_MODES_PERMISSIONS );
+  Log.add( 'hasPermissions', permissionsGranted, true );
+
+  if ( ! permissionsGranted ) {
+    // @todo Figure out why browser.permissions.contains returns false for origins <all_urls>, whereas browser.permissions.getAll lists it. Related: {@link https://bugs.chromium.org/p/chromium/issues/detail?id=931816}
+    const {
+      [ ORIGINS_KEY ]: origins,
+      [ PERMISSIONS_KEY ]: permissions,
+    } = await browser.permissions.getAll();
+    const originsIncluded = origins.includes( ALL_URLS_ORIGIN );
+    const permissionsIncluded = permissions.includes( TABS_PERMISSION );
+
+    Log.add( 'hasPermissions fallback', {
+      origins,
+      originsIncluded,
+      permissions,
+      permissionsIncluded,
+    }, true );
+
+    return ( originsIncluded && permissionsIncluded );
+  }
+
+  return permissionsGranted;
 }
