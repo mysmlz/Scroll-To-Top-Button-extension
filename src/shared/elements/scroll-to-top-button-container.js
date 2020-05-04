a14caae49cbc9f3d6c888dfcfacbc036926d88df
PoziWorld <5961162+PoziWorld@users.noreply.github.com>
2020-05-03 23:34:36 -0700

    New: add self-defense mechanism against container style attr changes

--- src/shared/elements/scroll-to-top-button-container.js
+++ src/shared/elements/scroll-to-top-button-container.js
@@ -1,5 +1,7 @@
 import './scroll-to-top-button-container.css';
 
+import * as feedback from 'Shared/feedback';
+
 /**
  * Scroll To Top Button button container custom element to avoid style collisions.
  *
@@ -8,11 +10,71 @@ import './scroll-to-top-button-container.css';
  */
 
 export class ScrollToTopButtonContainer extends HTMLElement {
-  /**
-   * Define all the functionality the element will have when an instance of it is instantiated.
-   */
+  #FIRST_DETECTION_INDEX = 1;
+  #WARNING_COUNT = 10;
 
-  constructor() {
-    super();
+  #styleWatcher;
+  #styleChangesDetectedCount = 0;
+  #firstDetectionTimestamp;
+
+  connectedCallback() {
+    this.#watchStyleChanges();
   }
+
+  #watchStyleChanges() {
+    // @todo Figure out why “static get observedAttributes() { return [ 'data-default-background-color', 'style', ]; }” and “attributeChangedCallback() { this.#resetStyle(); }” didn't work
+    this.#styleWatcher = new MutationObserver( ( mutations ) => { this.#handleStyleAttributeChanges( mutations ); } );
+
+    this.#styleWatcher.observe( this, {
+      attributes: true,
+      attributeFilter: [
+        'style',
+      ],
+    } );
+  }
+
+  #stopWatchingStyleChanges() {
+    this.#styleWatcher.disconnect();
+  }
+
+  #handleStyleAttributeChanges( mutations ) {
+    if ( this.#isStyleEmpty( mutations ) ) {
+      return;
+    }
+
+    this.#styleChangesDetectedCount += 1;
+
+    if ( this.#styleChangesDetectedCount === this.#FIRST_DETECTION_INDEX ) {
+      this.#firstDetectionTimestamp = getTimestamp();
+    }
+    else if ( this.#styleChangesDetectedCount === this.#WARNING_COUNT ) {
+      const timeFromFirstDetection = getTimestamp() - this.#firstDetectionTimestamp;
+
+      const ISSUE_MESSAGE_JSON_KEY = 'containerStyleAttributeRepeatedChangesIssue';
+      // Don't translate, as this gets sent to developer
+      const ISSUE_TITLE = 'Repeated style attribute changes issue';
+      // Don't translate, as this gets sent to developer
+      const debuggingInformation = `
+Attempts so far: ${ this.#WARNING_COUNT }
+Milliseconds since first attempt: ${ new Intl.NumberFormat().format( timeFromFirstDetection ) }
+Current URL: ${ window.location.href }`;
+
+      feedback.requestToReportIssue( ISSUE_MESSAGE_JSON_KEY, ISSUE_TITLE, debuggingInformation );
+      this.#stopWatchingStyleChanges();
+    }
+
+    this.#resetStyle();
+  }
+
+  #isStyleEmpty( mutations ) {
+    return ! poziworldExtension.utils.isNonEmptyString( mutations[ 0 ].target.getAttribute( 'style' ) );
+  }
+
+  #resetStyle() {
+    this.removeAttribute( 'style' );
+  }
+}
+
+function getTimestamp() {
+  return new Date().getTime();
 }
