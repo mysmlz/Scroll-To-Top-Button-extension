8536a2f08ea643cec101f2b373edb03a057c990e
PoziWorld <5961162+PoziWorld@users.noreply.github.com>
2020-07-26 21:00:07 -0700

    New: provide a random anonymous installation ID*
    * Could be useful to differentiate issue reports from different users.

--- src/shared/utils/extension.js
+++ src/shared/utils/extension.js
@@ -1,10 +1,48 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const INSTALLATION_ID_STORAGE_KEY = 'installationId';
+
 /**
  * Use the platform API to get the extension file URL.
  *
  * @param {string} path
- * @return {string}
+ * @returns {string}
  */
 
 export function getUrl( path ) {
   return browser.runtime.getURL( path );
+}
+
+/**
+ * Provide a random anonymous installation ID, which could be useful to differentiate issue reports from different users.
+ *
+ * @returns {string|'n/a'}
+ */
+
+export async function getInstallationId() {
+  try {
+    let { [ INSTALLATION_ID_STORAGE_KEY ]: installationId } = await browser.storage.local.get( INSTALLATION_ID_STORAGE_KEY );
+
+    if ( poziworldExtension.utils.isNonEmptyString( installationId ) ) {
+      return installationId;
+    }
+    else {
+      installationId = uuidv4();
+
+      try {
+        browser.storage.local.set( {
+          [ INSTALLATION_ID_STORAGE_KEY ]: installationId,
+        } );
+      }
+      catch ( error ) {
+        // @todo
+      }
+
+      return installationId;
+    }
+  }
+  catch ( error ) {
+    // @todo
+    return 'n/a';
+  }
 }
