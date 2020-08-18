852737c760e1cb776702e59050f0c24d96beee72
PoziWorld <5961162+PoziWorld@users.noreply.github.com>
2020-08-17 23:41:48 -0700

    Improve: report would sometimes fail* | New: prompt user for email
    * Due to GET request URL string limit getting exceeded

--- src/shared/feedback/index.js
+++ src/shared/feedback/index.js
@@ -1,21 +1,44 @@
 import * as i18n from 'Shared/i18n';
-import * as tabs from 'Shared/tabs';
-
-const ISSUE_TITLE_PLACEHOLDER = '$ISSUE_TITLE$';
-const FEEDBACK_TEXT_PLACEHOLDER = '$FEEDBACK_TEXT$';
-const ISSUE_REPORTING_URL_WITH_PLACEHOLDERS = `https://docs.google.com/forms/d/e/1FAIpQLSf0LEXunDXcHaBNd8diJRkVFrZp4FwvbHh86xZTfieK2rHxKg/viewform?entry.1591633300=__other_option__&entry.1591633300.other_option_response=${ ISSUE_TITLE_PLACEHOLDER }&entry.326955045=${ FEEDBACK_TEXT_PLACEHOLDER }`;
 
 export async function requestToReportIssue( issueMessageJsonKey, issueTitle, debuggingInformation ) {
   const debuggingInformationConfirmation = await i18n.getMessage( 'debuggingInformationConfirmation', [ debuggingInformation ] );
   const dialogMessage = await i18n.getMessage( issueMessageJsonKey, [ debuggingInformationConfirmation ] );
+  // Prompting user for email for a follow-up
+  let email = window.prompt( dialogMessage );
 
-  if ( window.confirm( dialogMessage ) ) {
-    tabs.createOrUpdate( generateIssueReportingUrl( issueTitle, debuggingInformation ) );
+  if ( email !== null ) {
+    reportIssue( issueTitle, debuggingInformation, email );
   }
 }
 
-function generateIssueReportingUrl( issueTitle, feedbackText ) {
-  return ISSUE_REPORTING_URL_WITH_PLACEHOLDERS
-    .replace( ISSUE_TITLE_PLACEHOLDER, window.encodeURIComponent( issueTitle ) )
-    .replace( FEEDBACK_TEXT_PLACEHOLDER, window.encodeURIComponent( feedbackText ) );
+function reportIssue( issueTitle, feedbackText, email ) {
+  const form = document.createElement( 'form' );
+  const feedbackTypeInput = document.createElement( 'input' );
+  const feedbackTypeOtherInput = document.createElement( 'input' );
+  const feedbackInput = document.createElement( 'textarea' );
+  const emailInput = document.createElement( 'input' );
+
+  form.method = 'post';
+  form.action = 'https://docs.google.com/forms/d/e/1FAIpQLSf0LEXunDXcHaBNd8diJRkVFrZp4FwvbHh86xZTfieK2rHxKg/formResponse';
+  form.target = '_blank';
+
+  feedbackTypeInput.name = 'entry.1591633300';
+  feedbackTypeInput.value = '__other_option__';
+
+  feedbackTypeOtherInput.name = 'entry.1591633300.other_option_response';
+  feedbackTypeOtherInput.value = issueTitle;
+
+  feedbackInput.name = 'entry.326955045';
+  feedbackInput.value = feedbackText;
+
+  emailInput.name = 'entry.879531967';
+  emailInput.value = email;
+
+  form.append( feedbackTypeInput );
+  form.append( feedbackTypeOtherInput );
+  form.append( feedbackInput );
+  form.append( emailInput );
+  document.body.append( form );
+
+  form.submit();
 }
