70fa03c79a56f9f3bf6db41097230746d00f2546
PoziWorld <5961162+PoziWorld@users.noreply.github.com>
2020-09-16 01:39:05 -0700

    Improve: remove redundant IIFE & 'use strict' in context-menus/legacy.js

--- src/shared/context-menus/legacy.js
+++ src/shared/context-menus/legacy.js
@@ -1,189 +1,185 @@
-( function () {
-  'use strict';
+// Context menu items
+const SCROLL_TO_TOP_ONLY_BASIC_ID = 'scrollToTopOnlyBasic';
+const SEPARATOR_ID = 'separator';
+const OPTIONS_ID = 'options';
 
-  // Context menu items
-  const SCROLL_TO_TOP_ONLY_BASIC_ID = 'scrollToTopOnlyBasic';
-  const SEPARATOR_ID = 'separator';
-  const OPTIONS_ID = 'options';
+/**
+ * @type {string[]}
+ * @see {@link https://developer.chrome.com/extensions/contextMenus#type-ContextType}
+ */
 
-  /**
-   * @type {string[]}
-   * @see {@link https://developer.chrome.com/extensions/contextMenus#type-ContextType}
-   */
+const contexts = [
+  'all',
+];
 
-  const contexts = [
-    'all',
+let _this;
+
+setUp();
+
+/**
+ * Make the logic readily available.
+ */
+
+function setUp() {
+  exposeApi();
+}
+
+/**
+ * Create an instance of the context menus API and expose it to other parts of the extension.
+ */
+
+function exposeApi() {
+  sttb.contextMenus = new ContextMenus();
+}
+
+/**
+ * Define a ContextMenus constructor.
+ *
+ * @constructor
+ */
+
+function ContextMenus() {
+  setInstance( this );
+}
+
+/**
+ * Check settings to find out whether context menus should be created or removed.
+ */
+
+ContextMenus.prototype.init = function() {
+  strLog = 'sttb.contextMenus.init';
+  Log.add( strLog );
+
+  const _this = getInstance();
+
+  poziworldExtension.utils.getSettings( strLog, _this.toggle );
+};
+
+/**
+ * Create and show the appropriate context menus when enabled in Options.
+ * Otherwise, remove all.
+ *
+ * @param {object} settings - Key-value pairs.
+ */
+
+ContextMenus.prototype.toggle = function( settings ) {
+  const contextMenus = browser.contextMenus;
+
+  if ( contextMenus ) {
+    if ( poziworldExtension.utils.isType( settings, 'object' ) && ! Global.isEmpty( settings ) && settings.contextMenu === 'on' ) {
+      browser.contextMenus.removeAll()
+        .then( createContextMenu )
+        .then( addListeners );
+    }
+    else {
+      browser.contextMenus.removeAll();
+    }
+  }
+};
+
+function createContextMenu() {
+  const items = [
+    {
+      id: SCROLL_TO_TOP_ONLY_BASIC_ID,
+      title: poziworldExtension.i18n.getMessage( 'scrollToTopOnlyBasicContextMenuItemTitle' ),
+    },
+    {
+      id: SEPARATOR_ID,
+      type: 'separator',
+    },
+    {
+      id: OPTIONS_ID,
+      title: poziworldExtension.i18n.getMessage( 'optionsContextMenuItemTitle' ),
+    },
   ];
 
-  let _this;
-
-  setUp();
-
-  /**
-   * Make the logic readily available.
-   */
-
-  function setUp() {
-    exposeApi();
+  while ( items.length ) {
+    createContextMenuItem( items.shift() );
   }
+}
 
-  /**
-   * Create an instance of the context menus API and expose it to other parts of the extension.
-   */
+function createContextMenuItem( properties ) {
+  const mergedProperties = Object.assign( {
+    contexts: contexts,
+  }, properties );
 
-  function exposeApi() {
-    sttb.contextMenus = new ContextMenus();
+  try {
+    browser.contextMenus.create( mergedProperties );
   }
+  catch ( error ) {
+    const GLOBAL_LOG_MESSAGE_UPDATED = false;
 
-  /**
-   * Define a ContextMenus constructor.
-   *
-   * @constructor
-   */
-
-  function ContextMenus() {
-    setInstance( this );
-  }
-
-  /**
-   * Check settings to find out whether context menus should be created or removed.
-   */
-
-  ContextMenus.prototype.init = function() {
-    strLog = 'sttb.contextMenus.init';
-    Log.add( strLog );
-
-    const _this = getInstance();
-
-    poziworldExtension.utils.getSettings( strLog, _this.toggle );
-  };
-
-  /**
-   * Create and show the appropriate context menus when enabled in Options.
-   * Otherwise, remove all.
-   *
-   * @param {object} settings - Key-value pairs.
-   */
-
-  ContextMenus.prototype.toggle = function( settings ) {
-    const contextMenus = browser.contextMenus;
-
-    if ( contextMenus ) {
-      if ( poziworldExtension.utils.isType( settings, 'object' ) && ! Global.isEmpty( settings ) && settings.contextMenu === 'on' ) {
-        browser.contextMenus.removeAll()
-          .then( createContextMenu )
-          .then( addListeners );
+    Log.add(
+      'Failed to create context menu item',
+      {
+        error: error,
+        properties: properties,
+      },
+      GLOBAL_LOG_MESSAGE_UPDATED,
+      {
+        level: 'error',
       }
-      else {
-        browser.contextMenus.removeAll();
-      }
-    }
-  };
-
-  function createContextMenu() {
-    const items = [
-      {
-        id: SCROLL_TO_TOP_ONLY_BASIC_ID,
-        title: poziworldExtension.i18n.getMessage( 'scrollToTopOnlyBasicContextMenuItemTitle' ),
-      },
-      {
-        id: SEPARATOR_ID,
-        type: 'separator',
-      },
-      {
-        id: OPTIONS_ID,
-        title: poziworldExtension.i18n.getMessage( 'optionsContextMenuItemTitle' ),
-      },
-    ];
-
-    while ( items.length ) {
-      createContextMenuItem( items.shift() );
-    }
+    );
   }
+}
 
-  function createContextMenuItem( properties ) {
-    const mergedProperties = Object.assign( {
-      contexts: contexts,
-    }, properties );
+/**
+ * Context menus are actionable. Add a click listener.
+ */
 
-    try {
-      browser.contextMenus.create( mergedProperties );
-    }
-    catch ( error ) {
-      const GLOBAL_LOG_MESSAGE_UPDATED = false;
+function addListeners() {
+  browser.contextMenus.onClicked.addListener( handleContextMenuClick );
+}
 
-      Log.add(
-        'Failed to create context menu item',
-        {
-          error: error,
-          properties: properties,
-        },
-        GLOBAL_LOG_MESSAGE_UPDATED,
-        {
-          level: 'error',
-        }
-      );
-    }
+/**
+ * Fired when a context menu item is clicked.
+ *
+ * @param {object} info - Information about the item clicked and the context where the click happened.
+ */
+
+function handleContextMenuClick( info ) {
+  const menuItemId = info.menuItemId;
+
+  switch ( menuItemId ) {
+    case SCROLL_TO_TOP_ONLY_BASIC_ID:
+      triggerScrollToTopBasic();
+
+      break;
+    case OPTIONS_ID:
+      Global.openOptionsPage( menuItemId );
+
+      break;
   }
+}
 
-  /**
-   * Context menus are actionable. Add a click listener.
-   */
+/**
+ * Ask another extension component to scroll the active tab to top.
+ */
 
-  function addListeners() {
-    browser.contextMenus.onClicked.addListener( handleContextMenuClick );
-  }
+function triggerScrollToTopBasic() {
+  const TARGET_ORIGIN = '*';
 
-  /**
-   * Fired when a context menu item is clicked.
-   *
-   * @param {object} info - Information about the item clicked and the context where the click happened.
-   */
+  window.postMessage( {
+    trigger: SCROLL_TO_TOP_ONLY_BASIC_ID,
+  }, TARGET_ORIGIN );
+}
 
-  function handleContextMenuClick( info ) {
-    const menuItemId = info.menuItemId;
+/**
+ * Return the ContextMenus instance.
+ *
+ * @param {ContextMenus} instance
+ */
 
-    switch ( menuItemId ) {
-      case SCROLL_TO_TOP_ONLY_BASIC_ID:
-        triggerScrollToTopBasic();
+function setInstance( instance ) {
+  _this = instance;
+}
 
-        break;
-      case OPTIONS_ID:
-        Global.openOptionsPage( menuItemId );
+/**
+ * Return the ContextMenus instance.
+ *
+ * @return {ContextMenus}
+ */
 
-        break;
-    }
-  }
-
-  /**
-   * Ask another extension component to scroll the active tab to top.
-   */
-
-  function triggerScrollToTopBasic() {
-    const TARGET_ORIGIN = '*';
-
-    window.postMessage( {
-      trigger: SCROLL_TO_TOP_ONLY_BASIC_ID,
-    }, TARGET_ORIGIN );
-  }
-
-  /**
-   * Return the ContextMenus instance.
-   *
-   * @param {ContextMenus} instance
-   */
-
-  function setInstance( instance ) {
-    _this = instance;
-  }
-
-  /**
-   * Return the ContextMenus instance.
-   *
-   * @return {ContextMenus}
-   */
-
-  function getInstance() {
-    return _this;
-  }
-} )();
+function getInstance() {
+  return _this;
+}
